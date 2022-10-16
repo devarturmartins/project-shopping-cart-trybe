6063213928f29ba10c5dae661a5f60250e5ea067
devarturmartins <arturvl1@hotmail.com>
2022-10-16 18:32:24 -0300

    r. 6 e r.7

--- tests/saveCartItems.test.js
+++ tests/saveCartItems.test.js
@@ -5,5 +5,13 @@ localStorageSimulator('setItem');
 
 describe('3 - Teste a função saveCartItems', () => {
   // implemente seus testes aqui
-  fail('Teste vazio');
+  it('Teste se, ao executar saveCartItems com um cartItem como argumento, o método localStorage.setItem é chamado', async () => {
+    await saveCartItems('MLB1978768665');
+    expect(localStorage.setItem).toHaveBeenCalled();
+  });
+  it('Teste se, ao executar saveCartItems com um cartItem como argumento, o método localStorage.setItem é chamado com dois parâmetros, sendo o primeiro a chave cartItems e o segundo sendo o valor passado como argumento para saveCartItems.', async () => {
+    await saveCartItems('MLB1978768665')
+    expect(localStorage.setItem).toHaveBeenCalledWith('cartItems', 'MLB1978768665');
+  });
+  // fail('Teste vazio');
 });
